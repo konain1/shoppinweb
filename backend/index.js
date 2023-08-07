20246ec255ccc23116e119ec227077b3f56ed86b
konain1 <kaunainkasab@gmail.com>
2023-08-07 16:49:26 +0530

    backend listener 5001 port

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express')
+const cors = require("cors")
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("welcome to tamazon")
+
+})
+
+
+app.listen(5001)
