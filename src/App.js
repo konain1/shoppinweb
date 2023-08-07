79bfbbd16cc1e57412857c2908b15752274e3a15
konain1 <kaunainkasab@gmail.com>
2023-08-07 15:16:55 +0530

    router-dom installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRoute as Router , Route , Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+    <Router>
+    <Routes> 
+    <Route> </Route>
+    </Routes>
+
+    </Router>
       <h1>hellow orld</h1>
     </div>
   );
