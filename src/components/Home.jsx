a774664b3466b1872ca46f74a8b45ca566ba8e1f
konain1 <kaunainkasab@gmail.com>
2023-08-17 15:23:35 +0530

    showing the products on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,37 @@
 
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Home() {
+
+  const selected = useSelector((state)=> state.products.items)
+
+  console.log(selected)
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+
+    <>
+  <div style={{display:'flex'}}>
+
+  
+    {
+      selected?.map((item,index)=>{
+        return(
+          <div style={{
+            width:'300px' , height:'400px', border:'1px solid black' , margin:'20px 20px', display:'grid',placeItems:'center'
+          }}>
+
+          <div>{item.name}</div>
+          <div style={{}}> <img src={item.image}  style={{width:'140px',}}/></div>
+          <div>{item.price}</div>
+            
+          </div>
+        )
+      })
+    }
+
+  </div>
+    </>
+   
   )
 }
 
